22b4e5c9eb31af7a0f1a30f1f8dc284c41e6b9e0
Nicolas Aguirre <inaguirre96@gmail.com>
2023-08-18 09:49:34 -0400

    post working

--- components/ThreadsColMid/threads-col-container.js
+++ components/ThreadsColMid/threads-col-container.js
@@ -10,7 +10,7 @@ const ThreadsColContainerColumn = ({
   return (
     <div className={styles.threadscontainer}>
       <NewThreadInput
-        inputValue={setInputValue}
+        inputValue={inputValue}
         setInputValue={setInputValue}
         createThread={createThread}
       />
@@ -20,7 +20,7 @@ const ThreadsColContainerColumn = ({
             <ThreadContainer
               username={thread.username}
               timeSpentSincePosted={thread.date}
-              thread={thread.thread}
+              thread={thread.content}
               commentsShown={false}
             />
           );
@@ -30,30 +30,6 @@ const ThreadsColContainerColumn = ({
         timeSpentSincePosted="1min"
         thread={`guys, just see that now we have a "Save for Later" I've been waiting for this so much`}
         commentsShown={false}
-      />
-      <ThreadContainer
-        username="aura"
-        timeSpentSincePosted="2min"
-        thread="I’ve been exploring ways of setting up variables in Figma if you have two different sets of global colours for light and dark themes with multiple brands. If you want to learn more about it, DM me, please"
-        commentsShown
-      />
-      <ThreadContainer
-        username="aura"
-        timeSpentSincePosted="4min"
-        thread="Hold onto your seats, folks! The new iPhone is coming in hot, ready to rock our world! Imagine the stunning photos and videos I'll capture with this beauty! 📸💥 #BringItOnNewiPhone #BeyondExcited"
-        commentsShown={false}
-      />
-      <ThreadContainer
-        username="aura"
-        timeSpentSincePosted="2min"
-        thread="I’ve been exploring ways of setting up variables in Figma if you have two different sets of global colours for light and dark themes with multiple brands. If you want to learn more about it, DM me, please"
-        commentsShown
-      />
-      <ThreadContainer
-        username="aura"
-        timeSpentSincePosted="2min"
-        thread="I’ve been exploring ways of setting up variables in Figma if you have two different sets of global colours for light and dark themes with multiple brands. If you want to learn more about it, DM me, please"
-        commentsShown
       />
     </div>
   );
